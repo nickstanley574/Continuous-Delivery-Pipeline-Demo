9d1ffe420677be8ed6a5f3d2f9a4ec2fac184b5b
nick <40447503+nickstanley574@users.noreply.github.com>
2023-11-30 20:32:41 -0600

    selenium

--- app/app.py
+++ app/app.py
@@ -1,41 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-db = SQLAlchemy(app)
+import logging
 
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), nullable=False)
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    db = SQLAlchemy(app)
+
+    # log = logging.getLogger('werkzeug')
+    # log.setLevel(logging.ERROR)
+
+    class Task(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(50), nullable=False)
+
+    @app.route("/")
+    def index():
+        tasks = Task.query.all()
+        return render_template("index.html", tasks=tasks)
 
 
-@app.route("/")
-def index():
-    tasks = Task.query.all()
-    return render_template("index.html", tasks=tasks)
+    @app.route("/add", methods=["POST"])
+    def add():
+        title = request.form.get("title")
+        new_task = Task(title=title)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for("index"))
 
 
-@app.route("/add", methods=["POST"])
-def add():
-    title = request.form.get("title")
-    new_task = Task(title=title)
-    db.session.add(new_task)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-@app.route("/delete/<int:id>")
-def delete(id):
-    task_to_delete = db.session.get(Task, id)
-    db.session.delete(task_to_delete)
-    db.session.commit()
-    return redirect(url_for("index"))
+    @app.route("/delete/<int:id>")
+    def delete(id):
+        task_to_delete = db.session.get(Task, id)
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect(url_for("index"))
+    
+    return app, db
 
 
 if __name__ == "__main__":
+    app, db = create_app()
     with app.app_context():
         db.create_all()
     # app.run(debug=True)
-    app.run(debug=False)
+    app.run(debug=True, port=5000, host='0.0.0.0')
--- app/test_selenium.py
+++ app/test_selenium.py
@@ -1,51 +1,102 @@
 import unittest
 from selenium import webdriver
-
-# from selenium.webdriver.common.keys import Keys
+import docker
 import time
+import socket
+from contextlib import closing
+
+
+
+
+docker_client = docker.from_env()
+
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
 
 
 class SeleniumTestCase(unittest.TestCase):
+
     def setUp(self):
+
+        # Define the container name
+        self.container_name_pattern = 'cicd-demo-webapp-selenium-local-'
+
+        port = find_free_port()
+        self.container_name=f'{self.container_name_pattern}{port}'
+        self.container = docker_client.containers.run('cicd-demo-webapp:local', detach=True, name=self.container_name, ports={5000: port})
+
+        time.sleep(2)
+
         self.options = webdriver.ChromeOptions()
         self.driver = webdriver.Chrome(options=self.options)
+        self.driver.get(f'http://127.0.0.1:{port}')
+        self.driver.set_window_size(1024, 768)
 
-        self.driver.get("http://127.0.0.1:5000/")
-        time.sleep(5)
 
     def tearDown(self):
+        # logs = self.container.logs().decode('utf-8')
+        self.container.stop()
+        self.container.remove()
         self.driver.quit()
 
-    def test_add_task(self):
-        # Find the input field and submit button
-        input_element = self.driver.find_element_by_name("title")
-        submit_button = self.driver.find_element_by_xpath("//form/button")
-        time.sleep(3)
 
-        # Type a task into the input field and submit
+    @classmethod
+    def tearDownClass(self):
+        # Code that runs once after all test methods have been called
+        # Get a list of all containers
+        for container in docker_client.containers.list():
+            # Check if the container name contains the specified pattern
+            if self.container_name_pattern in container.name:
+                container.stop()
+                container.remove()
+                print(f"Container {container.name} stopped and removed.")
+
+
+
+    def test_add_task(self):
+        input_element = self.driver.find_element('name', "title")
+        submit_button = self.driver.find_element('xpath', "//form/button")
         input_element.send_keys("Selenium Test Task")
         submit_button.click()
-        time.sleep(3)
 
         # Verify that the task is added to the list
-        tasks = self.driver.find_elements_by_xpath("//ul/li")
+        tasks = self.driver.find_elements('xpath', "//ul/li/span")
         self.assertEqual(len(tasks), 1)
         self.assertEqual(tasks[0].text, "Selenium Test Task")
 
-    # def test_delete_task(self):
-    #     # Add a task for deletion
-    #     input_element = self.driver.find_element_by_name('title')
-    #     submit_button = self.driver.find_element_by_xpath('//form/button')
-    #     input_element.send_keys('Task to Delete')
-    #     submit_button.click()
+    def test_task_order(self):
 
-    #     # Find the delete link and click it
-    #     delete_link = self.driver.find_element_by_xpath('//ul/li/a')
-    #     delete_link.click()
+        r = range(1, 3)
 
-    #     # Verify that the task is deleted from the list
-    #     tasks = self.driver.find_elements_by_xpath('//ul/li')
-    #     self.assertEqual(len(tasks), 0)
+        for i in r:
+            input_element = self.driver.find_element('name', "title")
+            submit_button = self.driver.find_element('xpath', "//form/button")
+            input_element.send_keys(f"Selenium Test Task {i}")
+            submit_button.click() 
+
+        tasks = self.driver.find_elements('xpath', "//ul/li/span")
+
+        for i in r:
+            self.assertEqual(tasks[i-1].text, f"Selenium Test Task {i}")
+
+    def test_delete_task(self):
+        # Add a task for deletion
+        input_element = self.driver.find_element('name', "title")
+        submit_button = self.driver.find_element('xpath', "//form/button")
+        input_element.send_keys("Task to Delete")
+        submit_button.click()
+
+        # Find the delete link and click it
+        delete_link = self.driver.find_element('xpath' , '//ul/li/a')
+        delete_link.click()
+
+        # Verify that the task is deleted from the list
+        tasks = self.driver.find_elements('xpath', '//ul/li')
+        self.assertEqual(len(tasks), 0)
 
 
 if __name__ == "__main__":
